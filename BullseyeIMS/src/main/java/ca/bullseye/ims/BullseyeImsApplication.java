9fb49cc6e0727d2241d9bb009238029056b75afd
EllaMoldez <90626155+EllaMoldez@users.noreply.github.com>
2022-09-30 12:28:10 -0300

    Initial Update

---
+++ BullseyeIMS/src/main/java/ca/bullseye/ims/BullseyeImsApplication.java
@@ -0,0 +1,13 @@
+package ca.bullseye.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullseyeImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullseyeImsApplication.class, args);
+	}
+
+}
---
+++ BullseyeIMS/src/test/java/ca/bullseye/ims/BullseyeImsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bullseye.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullseyeImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
