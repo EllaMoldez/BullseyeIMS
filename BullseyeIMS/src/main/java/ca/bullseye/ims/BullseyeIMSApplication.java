1e47be3ad8314f69739b4eeac145125d48cd967f
TommyMclaughlin <90393906+TommyMclaughlin@users.noreply.github.com>
2022-10-17 14:43:29 -0300

    commit

--- BullseyeIMS/src/main/java/ca/bullseye/ims/BullseyeIMSApplication.java
+++ BullseyeIMS/src/main/java/ca/bullseye/ims/BullseyeIMSApplication.java
@@ -1,8 +1,18 @@
 package ca.bullseye.ims;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EntityScan("ca.bullseye.ims.model")
+@EnableJpaRepositories("ca.bullseye.ims.repositories")
 @SpringBootApplication
 public class BullseyeIMSApplication {
 
