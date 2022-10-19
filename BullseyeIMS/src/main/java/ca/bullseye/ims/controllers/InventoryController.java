d621703f3f54391e389357bc7fc385d1931438a2
PaulRamos007 <pramos01@mynbcc.ca>
2022-10-18 23:49:44 -0300

    Final Update on Inventory

--- BullseyeIMS/src/main/java/ca/bullseye/ims/controllers/InventoryController.java
+++ BullseyeIMS/src/main/java/ca/bullseye/ims/controllers/InventoryController.java
@@ -5,33 +5,76 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.bullseye.ims.model.Employee;
 import ca.bullseye.ims.model.Inventory;
+import ca.bullseye.ims.model.Orders;
 import ca.bullseye.ims.model.Product;
 import ca.bullseye.ims.model.Supplier;
+import ca.bullseye.ims.services.EmployeeService;
 import ca.bullseye.ims.services.InventoryService;
+import ca.bullseye.ims.services.OrderService;
+import ca.bullseye.ims.services.ProductService;
 
 @Controller
 public class InventoryController {
 
 	@Autowired
-	private InventoryService inventoryService;
+	private EmployeeService employeeService;
+	@Autowired
+	private ProductService productService;
+	@Autowired
+	private OrderService orderService;
+
+	private List<Employee> employeeList;
+	private List<Product> productList;
+	private List<Orders> orderList;
+
+	@ModelAttribute("currentInventories")
+	public Inventory currentInventories() {
+		return new Inventory();
+	}
+	
+	@ModelAttribute("orderList")
+	public List<Orders> populateOrderList() {
+		orderList = orderService.getAllOrders();
+		return orderList;
+	}
+
+	@ModelAttribute("productList")
+	public List<Product> populateProductList() {
+		productList = productService.getAllProducts();
+		return productList;
+	}
+
+	@ModelAttribute("employeeList")
+	public List<Employee> populateEmployeeList() {
+		employeeList = employeeService.getAllEmployees();
+		return employeeList;
+	}
 
 	@GetMapping("/inventory")
-	public String getInventoryPage(Model model, HttpServletRequest request) {
+	public String getInventoryPage(@RequestParam(value = "search", required = false) String search,
+			@RequestParam(defaultValue = "inventoryId") String sort, @RequestParam(defaultValue = "1") int page,
+			@RequestParam(defaultValue = "15") int size, @RequestParam(defaultValue = "asc") String direction,
+			Model model, HttpServletRequest request) {
 		if (request.getSession().getAttribute("loggedIn") != null) {
-			List<Inventory> inventoryList = inventoryService.getAllInventories();
-//			List<Product> productList = inventoryService.getAllProducts();
-//			List<Employee> employeeList = inventoryService.getAllEmployees();
-//			List<Supplier> supplierList = inventoryService.getAllSuppliers();
-			model.addAttribute("inventoryList", inventoryList);
-//			model.addAttribute("productList", productList);
-//			model.addAttribute("employeeList", employeeList);
-//			model.addAttribute("supplierList", supplierList);
+
+			model.addAttribute("currentPage", page);
+			model.addAttribute("pageSize", size);
+			model.addAttribute("employeeList", employeeList);
+			model.addAttribute("productList", productList);
+			model.addAttribute("orderList", orderList);
+		
 			return "inventory";
 		}
 		return "login";
--- BullseyeIMS/src/main/java/ca/bullseye/ims/model/Inventory.java
+++ BullseyeIMS/src/main/java/ca/bullseye/ims/model/Inventory.java
@@ -1,68 +1,107 @@
 package ca.bullseye.ims.model;
 
-import java.util.List;
+import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
 
 @Entity
 @Table(name = "inventory")
 public class Inventory {
-
-	@OneToMany
-	@JoinColumn(name = "employee_empId")
-	private List<Employee> employees;
-
-	@OneToMany
-	@JoinColumn(name = "product_prodId")
-	private List<Product> products;
-
-	@OneToMany
-	@JoinColumn(name = "supplier_supId")
-	private List<Supplier> suppliers;
 	
-//	@OneToMany
-//	@JoinColumn(name = "order_orderId")
-//	private List<Order> orders;
-	
-	public List<Employee> getEmployees() {
-		return employees;
-	}
-
-	public void setEmployees(List<Employee> employees) {
-		this.employees = employees;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<Product> products) {
-		this.products = products;
-	}
-
-	public List<Supplier> getSuppliers() {
-		return suppliers;
-	}
-
-	public void setSuppliers(List<Supplier> suppliers) {
-		this.suppliers = suppliers;
-	}
-
-//	public List<Order> getOrders() {
-//		return orders;
-//	}
-//
-//	public void setOrders(List<Order> orders) {
-//		this.orders = orders;
-//	}
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long inventoryId;
+
+	@Column(name="prodSKU")
+	private String prodSKU;
+
+	@Column(name="prodName")
+	private String prodName;
+	
+	@Column(name="prodCategory")
+	private String prodCategory;
+	
+	@Digits(integer = 19, fraction = 2)
+	@Column(name="prodQuantity")
+	private BigDecimal prodQuantity;
+	
+	@Column(name="prodSupplier")
+	private String prodSupplier;
+	
+	@Digits(integer = 19, fraction = 2)
+	@Column(name="prodPrice")
+	private BigDecimal prodPrice;
+	
+	@Column(name="prodStatus")
+	private String prodStatus;
+
+	public Long getInventoryId() {
+		return inventoryId;
+	}
+
+	public void setInventoryId(Long inventoryId) {
+		this.inventoryId = inventoryId;
+	}
+
+	public String getProdSKU() {
+		return prodSKU;
+	}
+
+	public void setProdSKU(String prodSKU) {
+		this.prodSKU = prodSKU;
+	}
+
+	public String getProdName() {
+		return prodName;
+	}
+
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+	public String getProdCategory() {
+		return prodCategory;
+	}
+
+	public void setProdCategory(String prodCategory) {
+		this.prodCategory = prodCategory;
+	}
+
+	public @Digits(integer = 19, fraction = 2) BigDecimal getProdQuantity() {
+		return prodQuantity;
+	}
+
+	public void setProdQuantity(@Digits(integer = 19, fraction = 2) BigDecimal prodQuantity) {
+		this.prodQuantity = prodQuantity;
+	}
+
+	public String getProdSupplier() {
+		return prodSupplier;
+	}
+
+	public void setProdSupplier(String prodSupplier) {
+		this.prodSupplier = prodSupplier;
+	}
+
+	public @Digits(integer = 19, fraction = 2) BigDecimal getProdPrice() {
+		return prodPrice;
+	}
+
+	public void setProdPrice(@Digits(integer = 19, fraction = 2) BigDecimal prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+
+	public String getProdStatus() {
+		return prodStatus;
+	}
+
+	public void setProdStatus(String prodStatus) {
+		this.prodStatus = prodStatus;
+	}
 }
